3f38ffa3702e7cba06107d0e8409a56a97e9f697
Jaime Barnhill <barnhijl@gmail.com>
2021-04-19 10:19:55 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+// Jaime Dano (Barnhill)
+// CIS 1202 101
+// 4/19/2021
+// Description: a function to calculate character offsets, throwing exceptions in error conditions
+
+#include <iostream>  //console
+#include <iomanip>   //formating cout
+#include <string> // String stuff
+
+using namespace std;
+
+//Function protocol
+char character(char start, int offset);
+
+int main() {
+
+
+
+
+	// END Program
+	system("pause");
+	return 0;
+}
+
+
+char character(char start, int offset) {
+
+	return 'a';
+}
