becfe5260751054113e42485b7a8f2c4d5bef6c7
Jaime Barnhill <barnhijl@gmail.com>
2021-04-22 12:32:44 -0400

    Changed original source files name and added base for the second part of the assignment

---
+++ mathStuff.cpp
@@ -0,0 +1,20 @@
+// Jaime Dano (Barnhill)
+// CIS 1202 101
+// 4/22/2021
+// Description: templated functions to perform math on varying numeric data types
+
+#include <iostream>  //console
+#include <iomanip>   //formating cout
+#include <string>  //String stuff - possibly not needed
+#include <cmath> // Math
+
+using namespace std;
+
+int main() {
+
+
+
+	// END Program
+	system("pause");
+	return 0;
+}
