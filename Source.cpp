32e15cee0e75f2f1ac981574d54c2e972ed182f4
Jaime Barnhill <barnhijl@gmail.com>
2021-04-22 13:01:35 -0400

    Moved everything into source.cpp, changed the main function to take a tiny menu for both assignments, and split those into their own functions. 15b is almost working but int is still rounding down.

---
+++ Source.cpp
@@ -0,0 +1,143 @@
+// Jaime Dano (Barnhill)
+// CIS 1202 101
+// 4/22/2021
+// Description: a function to calculate character offsets, throwing exceptions in error conditions
+
+#include <iostream>  //console
+#include <iomanip>   //formating cout
+#include <string>  //String stuff - possibly not needed
+#include <cmath>
+
+using namespace std;
+
+//Function Protocols
+double half(double);
+float half(float);
+int half(int);
+char character(char start, int offset);
+
+//Main function protocols
+int math_stuff();
+int exception_assign();
+
+int main() {
+
+	char assignment;
+	
+	do {
+		//Which assignment?
+		cout << "Which assignment?" << endl;
+		cout << "a. 15a" << endl << "b. 15b" << endl << "q. Quit" << endl;
+		cin >> assignment;
+
+		//Call 1st assignment
+		if (assignment == 'a') {
+			exception_assign();
+		}
+
+		//Call 2nd assignment
+		else if (assignment == 'b') {
+			math_stuff();
+		}
+
+		//Invalid
+		else {
+			cout << "Invalid, try again." << endl;
+		}
+
+	} while (assignment != 'q');
+
+	// END Program
+	system("pause");
+	return 0;
+
+}
+
+//15b ------------------------------
+int math_stuff() {
+
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << "Half of " << a << " is " << half(a) << endl;
+	cout << "Half of " << b << " is " << half(b) << endl;
+	cout << "Half of " << c << " is " << half(c) << endl;
+
+	// END Program
+	system("pause");
+	return 0;
+}
+
+double half(double d) {
+	return d / 2;
+}
+
+float half(float d) {
+	return d / 2;
+}
+
+int half(int d) {
+	return ceil(d / 2);
+}
+
+//15a ------------------------------
+int exception_assign() {
+
+	//Variables
+	char the_char, new_char;
+	int the_int;
+
+	//Get input to test
+	cout << "Type a char:" << endl;
+	cin >> the_char;
+	cout << "Type an int:" << endl;
+	cin >> the_int;
+
+	try {
+		//Check if input is a letter and if not throw to the exception
+		if (isalpha(the_char)) {
+			new_char = character(the_char, the_int);
+		}
+		else {
+			throw (the_char);
+		}
+	}
+	catch (char wrong_char) {
+		cout << "InvalidCharacterException" << endl;
+		// END Program - otherwise below code gets mad
+		system("pause");
+		return 0;
+	}
+
+	//Work with the new character
+	try {
+		//If it's a letter print a message to confirm for testing and if not throw it
+		if (isalpha(new_char)) {
+			cout << new_char << " is the new char" << endl;
+		}
+		else {
+			throw (new_char);
+		}
+	}
+	//Print the error message and the wrong char
+	catch (char wrong_char) {
+		cout << "invalidRangeException: " << wrong_char << endl;
+	}
+
+	// END Program
+	system("pause");
+	return 0;
+}
+
+//Change the Char to an int, to the offset, and then back to a char
+char character(char start, int offset) {
+	//Variables
+	int new_num;
+	char new_char;
+
+	//Assign new char based on the offset and then return the new char
+	new_num = int(start) + offset;
+	new_char = char(new_num);
+	return new_char;
+}
--- mathStuff.cpp
+++ mathStuff.cpp
@@ -3,18 +3,5 @@
 // 4/22/2021
 // Description: templated functions to perform math on varying numeric data types
 
-#include <iostream>  //console
-#include <iomanip>   //formating cout
-#include <string>  //String stuff - possibly not needed
-#include <cmath> // Math
+//Empty now
 
-using namespace std;
-
-int main() {
-
-
-
-	// END Program
-	system("pause");
-	return 0;
-}
