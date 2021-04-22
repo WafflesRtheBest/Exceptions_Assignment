b93750c60f817d589c6eb7ad0c28d7d82f6e7ec9
Jaime Barnhill <barnhijl@gmail.com>
2021-04-22 12:25:10 -0400

    Fixed the earlier error by ending the program before the second Try-block if the exception was encountered, added comments and code runs as intended.

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>  //console
 #include <iomanip>   //formating cout
-#include <string> // String stuff
+#include <string>  //String stuff - possibly not needed
 
 using namespace std;
 
@@ -18,14 +18,14 @@ int main() {
 	char the_char, new_char; 
 	int the_int;
 
-	//Get and test input
+	//Get input to test
 	cout << "Type a char:" << endl;
 	cin >> the_char;
 	cout << "Type an int:" << endl;
 	cin >> the_int;
 
 	try {
-		
+		//Check if input is a letter and if not throw to the exception
 		if (isalpha(the_char)) {
 			new_char = character(the_char, the_int);
 		}
@@ -33,13 +33,16 @@ int main() {
 			throw (the_char);
 		}
 	}
-	catch (char new_char) {
-		cout << "InvalidCharacterException" << endl;
+	catch (char wrong_char) {
+		cout << "InvalidCharacterException" << endl; 
+		// END Program - otherwise below code gets mad
+		system("pause");
+		return 0;
 	}
 
 	//Work with the new character
 	try {
-
+		//If it's a letter print a message to confirm for testing and if not throw it
 		if (isalpha(new_char)) {
 			cout << new_char << " is the new char" << endl;
 		}
@@ -47,6 +50,7 @@ int main() {
 			throw (new_char);
 		}
 	}
+	//Print the error message and the wrong char
 	catch (char wrong_char) {
 		cout << "invalidRangeException: " << wrong_char << endl;
 	}
@@ -58,9 +62,11 @@ int main() {
 
 //Change the Char to an int, to the offset, and then back to a char
 char character(char start, int offset) {
+	//Variables
 	int new_num;
 	char new_char;
 
+	//Assign new char based on the offset and then return the new char
 	new_num = int(start) + offset;
 	new_char = char(new_num);
 	return new_char;
