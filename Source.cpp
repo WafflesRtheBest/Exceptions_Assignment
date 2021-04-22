c6bdc3a88e1249aa669b605c01fbd68cbc83313c
Jaime Barnhill <barnhijl@gmail.com>
2021-04-22 12:09:46 -0400

    Works, but has a Run-Time Check Failure #3 - The variable 'new_char' is being used without being initialized. Code needs commented and error needs examined

--- Source.cpp
+++ Source.cpp
@@ -13,17 +13,55 @@ using namespace std;
 char character(char start, int offset);
 
 int main() {
+	
+	//Variables
+	char the_char, new_char; 
+	int the_int;
 
+	//Get and test input
+	cout << "Type a char:" << endl;
+	cin >> the_char;
+	cout << "Type an int:" << endl;
+	cin >> the_int;
 
+	try {
+		
+		if (isalpha(the_char)) {
+			new_char = character(the_char, the_int);
+		}
+		else {
+			throw (the_char);
+		}
+	}
+	catch (char new_char) {
+		cout << "InvalidCharacterException" << endl;
+	}
 
+	//Work with the new character
+	try {
+
+		if (isalpha(new_char)) {
+			cout << new_char << " is the new char" << endl;
+		}
+		else {
+			throw (new_char);
+		}
+	}
+	catch (char wrong_char) {
+		cout << "invalidRangeException: " << wrong_char << endl;
+	}
 
 	// END Program
 	system("pause");
 	return 0;
 }
 
-
+//Change the Char to an int, to the offset, and then back to a char
 char character(char start, int offset) {
+	int new_num;
+	char new_char;
 
-	return 'a';
+	new_num = int(start) + offset;
+	new_char = char(new_num);
+	return new_char;
 }
